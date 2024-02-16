079ce7aa999ee92f9adbba86f09d8b342c18bee1
han0won <han00won@gmail.com>
2024-02-16 18:21:14 +0900

    0216

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -2,18 +2,13 @@ new fullpage('#fullpage', {
     anchors: ['intro', 'about-me', 'portfolio-1', 'portfolio-2', 'more-info']
 });
 
-document.body.addEventListener("mousemove", (evt) => {
-    const mouseX = evt.clientX;
-    const mouseY = evt.clientY;
-  
-    gsap.set(".cursor", {
-      x: mouseX,
-      y: mouseY
-    });
-  
-    gsap.to(".shape", {
-      x: mouseX,
-      y: mouseY,
-      stagger: -0.1
+$(document).ready(function(){
+    $('.emoji-slick').slick({
+      autoplay: true,
+      autoplaySpeed: 1000,
+      vertical: true,
+      infinite: true, 
+      prevArrow: false,
+      nextArrow: false,
     });
 });
