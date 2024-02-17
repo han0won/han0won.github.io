c390d725383a6ddfbf5566544d2583fb440b6e3d
han0won <han00won@gmail.com>
2024-02-17 15:17:56 +0900

    0217

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -2,6 +2,13 @@ new fullpage('#fullpage', {
     anchors: ['intro', 'about-me', 'portfolio-1', 'portfolio-2', 'more-info']
 });
 
+$(window).on("mousemove", (e) => {
+  $(".cursor-default").css({
+      top: e.pageY + "px",
+      left: e.pageX + "px"
+  });
+});
+
 document.body.addEventListener("mousemove", evt => {
   const mouseX = evt.clientX;
   const mouseY = evt.clientY;
@@ -29,3 +36,23 @@ $(document).ready(function(){
     });
 });
 
+function initparticles() {
+  confetti();
+}
+
+function confetti() {
+  $.each($(".particletext.confetti"), function(){
+     var confetticount = ($(this).width()/50)*10;
+     for(var i = 0; i <= confetticount; i++) {
+        $(this).append('<span class="particle c' + $.rnd(1,2) + '" style="top:' + $.rnd(10,50) + '%; left:' + $.rnd(0,100) + '%;width:' + $.rnd(6,8) + 'px; height:' + $.rnd(3,4) + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+     }
+  });
+}
+
+jQuery.rnd = function(m,n) {
+  m = parseInt(m);
+  n = parseInt(n);
+  return Math.floor( Math.random() * (n - m + 1) ) + m;
+}
+
+initparticles();
