6096e1461caf5c3565bec4482748e02120b07126
han0won <han00won@gmail.com>
2024-02-17 13:26:07 +0900

    2차 중간저장

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -2,6 +2,22 @@ new fullpage('#fullpage', {
     anchors: ['intro', 'about-me', 'portfolio-1', 'portfolio-2', 'more-info']
 });
 
+document.body.addEventListener("mousemove", evt => {
+  const mouseX = evt.clientX;
+  const mouseY = evt.clientY;
+  
+  gsap.set(".cursor", {
+    x: mouseX,
+    y: mouseY
+  })
+  
+  gsap.to(".shape", {
+    x: mouseX,
+    y: mouseY,
+    stagger: -0.1
+  })
+})
+
 $(document).ready(function(){
     $('.emoji-slick').slick({
       autoplay: true,
@@ -12,3 +28,4 @@ $(document).ready(function(){
       nextArrow: false,
     });
 });
+
